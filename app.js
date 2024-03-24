c629febad03b13c1b6bfe79f21fc784a73b40a99
Heidi Joutsijoki <heidi20151@student.hamk.fi>
2024-03-24 17:01:01 +0200

    Deployment

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ mongoose
   })
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.tokenExtractor)
 app.use('/api/blogs', middleware.userExtractor, blogsRouter)
